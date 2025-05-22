064b316ac4e860f0527d706ac8c505d596014d0d
Nhat Anh <thanhchem.pro@gmail.com>
2025-05-22 22:26:32 +0700

    feat: Time Util

---
+++ src/ultil/time.c
@@ -0,0 +1,11 @@
+#define CPU_FREQ_MHZ 1000  // 1 GHz = 1000 MHz
+
+void delay_ms(unsigned int ms) {
+    volatile unsigned int count;
+    while (ms--) {
+        count = CPU_FREQ_MHZ * 1000;  // Approx 1 million cycles per ms
+        while (count--) {
+            __asm__ volatile("nop");
+        }
+    }
+}
