69432ce79938740aa5501521e4f58df9efedda8a
Milkyway Galaxy <blannkisblank@gmail.com>
2025-05-23 23:08:42 +0700

    test: cli history

--- src/util/stringUtil.h
+++ src/util/stringUtil.h
@@ -6,3 +6,7 @@ int startsWith(char *full, char *prefix);
 int is_all_digits(const char *str);
 
 int my_atoi(const char *str);
+
+void strCopy(char *dest, const char *src);
+
+int strLen(const char *str);
