b83bf5a215c978bddcb2d8114baada8989e7dea0
Liv_ Parker <thanhcua04@gmail.com>
2025-05-23 23:14:37 +0700

    Revert "test: cli history"
    This reverts commit 69432ce79938740aa5501521e4f58df9efedda8a.

--- src/cli/cli.c
+++ src/cli/cli.c
@@ -14,18 +14,18 @@ static int cbIndex = 0; // pointer of command buffer
 // Tab function varriable
 static char *commands[] = {"help", "clear", "showinfo", "baudRate", "handShake", "teamDisplay", "videoDisplay"};
 static int NUM_COMMANDS = (sizeof(commands) / sizeof(commands[0]));
-static char *matched = 0;    // place holder for the matched checking
+static char *matched = 0; // place holder for the matched checking
 static int matchedFound = 0; // Flag to check if matched found
 
 // History varriable
-static char history[MAX_HISTORY][MAX_COMMAND_SIZE];
+static char *history[MAX_HISTORY][MAX_COMMAND_SIZE];
 static int historyCount = 0;
 static int historyIndex = -1;
 
-void cli_welcome()
-{
+
+void cli_welcome() {
     uart_puts(
-        "==============================================================================================================\n\n");
+            "==============================================================================================================\n\n");
     uart_puts("8888888888 8888888 Y88b   d88P 8888888 888b    888  .d8888b.      .d8888b.   .d88888b.   .d88888b.  8888888b.  \n");
     uart_puts("888          888    Y88b d88P    888   8888b   888 d88P  Y88b    d88P  Y88b d88P   Y88b d88P   Y88b 888   Y88b \n");
     uart_puts("888          888     Y88o88P     888   88888b  888 888    888    888    888 888     888 888     888 888    888 \n");
@@ -44,185 +44,120 @@ void cli_welcome()
     uart_puts("  - Tran Quang Minh    | ID: s3988776\r\n");
     uart_puts("  - Vu Thien Minh Hao  | ID: s3938011\r\n");
     uart_puts(
-        "==============================================================================================================\n");
+            "==============================================================================================================\n");
     uart_puts("\n");
     uart_puts(myOs);
 }
 
-void cli_process()
-{
+void cli_process() {
     // Get the user input
     char c = uart_getc();
 
-    // Process the user input base on different case
-    switch (c)
-    {
+    //Process the user input base on different case
+    switch (c) {
 
-    // User using autofill by pressing tab
-    case '\t':
-        // Loop to check the matched command
-        for (int i = 0; i < NUM_COMMANDS; i++)
-        {
-            if (startsWith(commands[i], commandBuffer))
-            {
-                matched = commands[i];
-                matchedFound = 1;
+        // User using autofill by pressing tab
+        case '\t':
+            // Loop to check the matched command 
+            for (int i = 0; i < NUM_COMMANDS; i++) {
+                if (startsWith(commands[i], commandBuffer)) {
+                    matched = commands[i];
+                    matchedFound = 1;
+                    break;
+                }
+            }
+
+            // If nothing matched then return
+            if (!matchedFound) {
+                return;
+            }
+
+            // Clear the display
+            clearDisplay();
+
+            // clear the buffer
+            clearBuff(1);
+
+            // Promt the matched command;
+            for (int i = 0; matched[i] != '\0'; i++) {
+                commandBuffer[cbIndex++] = matched[i];
+                uart_sendc(matched[i]);
+            }
+
+            matchedFound = 0; // reset the flag
+            break;
+            // User press ENTER
+        case '\n':
+            // Call the commands processer function
+            cmdProcess(commandBuffer);
+
+            // Save to history
+            // if (historyCount < MAX_HISTORY) {
+            //         copyString(history[historyCount], commandBuffer);
+            //         historyCount++;
+            //     } else {
+            //         // Shift up and insert at end
+            //         for (int i = 1; i < MAX_HISTORY; i++) {
+            //             copyString(history[i - 1], history[i]);
+            //         }
+            //         copyString(history[MAX_HISTORY - 1], commandBuffer);
+            //     }
+            //     historyIndex = historyCount;
+
+            // Clear buffer after process the command 
+            clearBuff(0);
+
+            break;
+
+            // User delete character
+        case 127:
+        case '\b':
+            // Nothing to delete
+            if (cbIndex == 0) {
                 break;
             }
-        }
 
-        // If nothing matched then return
-        if (!matchedFound)
-        {
-            return;
-        }
+            // Visual display delete for user
+            uart_sendc('\b');
+            uart_sendc(' ');
+            uart_sendc('\b');
 
-        // Clear the display
-        clearDisplay();
-
-        // clear the buffer
-        clearBuff(1);
-
-        // Promt the matched command;
-        for (int i = 0; matched[i] != '\0'; i++)
-        {
-            commandBuffer[cbIndex++] = matched[i];
-            uart_sendc(matched[i]);
-        }
-
-        matchedFound = 0; // reset the flag
-        break;
-        // User press ENTER
-    case '\n':
-        // Call the commands processer function
-        cmdProcess(commandBuffer);
-
-        if (cbIndex > 0)
-        {
-            if (historyCount < MAX_HISTORY)
-            {
-                strCopy(history[historyCount++], commandBuffer);
-            }
-            else
-            {
-                for (int i = 1; i < MAX_HISTORY; i++)
-                {
-                    strCopy(history[i - 1], history[i]);
-                }
-                strCopy(history[MAX_HISTORY - 1], commandBuffer);
-            }
-        }
-        historyIndex = historyCount;
-
-        // Clear buffer after process the command
-        clearBuff(0);
-
-        break;
-
-        // User delete character
-    case 127:
-    case '\b':
-        // Nothing to delete
-        if (cbIndex == 0)
-        {
-            break;
-        }
-
-        // Visual display delete for user
-        uart_sendc('\b');
-        uart_sendc(' ');
-        uart_sendc('\b');
-
-        // Modify the buffer
-        cbIndex--;
-        commandBuffer[cbIndex] = '\0';
-        break;
-
-        // Normal input
-    default:
-        // Maximum command size reached
-        if (cbIndex > MAX_COMMAND_SIZE - 1)
-        {
-            break; // do nothing
-        }
-        // Add the new character to the buffer
-        commandBuffer[cbIndex] = c;
-        cbIndex++;
-        // Print the user input to the terminal
-        uart_sendc(c);
-
-        if (c == 0x1B)
-        { // ESC
-            char next = uart_getc();
-            if (next == '[')
-            {
-                char dir = uart_getc();
-                if (dir == 'A')
-                { // Up arrow
-                    if (historyIndex > 0)
-                    {
-                        historyIndex--;
-                        clearDisplay();
-                        clearBuff(1);
-                        strCopy(commandBuffer, history[historyIndex]);
-                        cbIndex = strLen(commandBuffer);
-                        uart_puts(commandBuffer);
-                    }
-                    return;
-                }
-                else if (dir == 'B')
-                { // Down arrow
-                    if (historyIndex < historyCount - 1)
-                    {
-                        historyIndex++;
-                        clearDisplay();
-                        clearBuff(1);
-                        strCopy(commandBuffer, history[historyIndex]);
-                        cbIndex = strLen(commandBuffer);
-                        uart_puts(commandBuffer);
-                    }
-                    else
-                    {
-                        clearDisplay();
-                        clearBuff(1);
-                    }
-                    return;
-                }
-            }
-        }
-
-        if (cbIndex < MAX_COMMAND_SIZE - 1)
-        {
-            commandBuffer[cbIndex++] = c;
+            // Modify the buffer
+            cbIndex--;
             commandBuffer[cbIndex] = '\0';
+            break;
+
+            // Normal input
+        default:
+            // Maximum command size reached
+            if (cbIndex > MAX_COMMAND_SIZE - 1) {
+                break; // do nothing
+            }
+            // Add the new character to the buffer
+            commandBuffer[cbIndex] = c;
+            cbIndex++;
+            // Print the user input to the terminal
             uart_sendc(c);
-        }
-        break;
     }
 }
 
 // Clear buffer and create now input line
-void clearBuff(int isTab)
-{
+void clearBuff(int isTab) {
     // Clear the command buffer and reset the index
-    for (int i = 0; i < cbIndex; i++)
-    {
+    for (int i = 0; i < cbIndex; i++) {
         commandBuffer[i] = '\0';
     }
     cbIndex = 0;
 
     // if the function called in the tab then dont print the next os
-    if (isTab)
-        return;
+    if (isTab) return;
     uart_sendc('\n');
     uart_puts(myOs);
 }
 
 // clear all the visual inputed on the screen
-void clearDisplay()
-{
-    for (int i = 0; i < cbIndex; i++)
-    {
+void clearDisplay() {
+    for (int i = 0; i < cbIndex; i++) {
         uart_sendc('\b'); // move back
         uart_sendc(' ');  // overwrite with space
         uart_sendc('\b'); // move back again
