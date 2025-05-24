0db3608628460a9d8c70e9cc08dc226ada152b6e
Milkyway Galaxy <blannkisblank@gmail.com>
2025-05-25 00:04:47 +0700

    update

--- src/assets/videoFrames/AK/AK47Video_part2.h
+++ src/assets/videoFrames/AK/AK47Video_part2.h
@@ -1,8 +1,4 @@
 //
 // Created by Admin on 5/24/2025.
 //
-
-#ifndef EMBEDDED3_GROUP_AK47VIDEO_H
-#define EMBEDDED3_GROUP_AK47VIDEO_H
-const unsigned long *AK_part2_allArray[25];
-#endif // EMBEDDED3_GROUP_AK47VIDEO_H
+const unsigned int *AK_part2_allArray[25];
