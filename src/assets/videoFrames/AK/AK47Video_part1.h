be4e453e19d2ea8a8206b158c9d28690d34d77d5
Milkyway Galaxy <blannkisblank@gmail.com>
2025-05-24 23:34:05 +0700

    update: add AK video

---
+++ src/assets/videoFrames/AK/AK47Video_part1.h
@@ -0,0 +1,8 @@
+//
+// Created by Admin on 5/24/2025.
+//
+
+#ifndef EMBEDDED3_GROUP_AK47VIDEO_H
+#define EMBEDDED3_GROUP_AK47VIDEO_H
+const unsigned long *AK_part1_allArray[25];
+#endif // EMBEDDED3_GROUP_AK47VIDEO_H
