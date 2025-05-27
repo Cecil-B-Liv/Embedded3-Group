dd0ee63136b4c214d4d9fc4128cfa6a9c2ffe770
Nhat Anh <91367670+NABlackFox@users.noreply.github.com>
2025-05-27 10:50:45 +0700

    chore: update comment for the game.c

--- src/game/game.c
+++ src/game/game.c
@@ -62,30 +62,33 @@ static volatile GameObject player = {.type = PLAYER_TAG,
                                      .width = PLAYER_WIDTH,
                                      .speed = PLAYER_SPEED,
                                      .alive = 1,
-                                     .sprite = basketball_hoops};
+                                     .sprite = basketball_hoops}; // Player object
 
 static int score = 0;
-static int frameCount = 0;            // frames
+static int frameCount = 0;            // frames count, use as base for all timer
 static int timerCount = STAGE1_TIME;  // second
 
-static int x2ScoreTimeCount = 0;
-static int x2ScoreActive = 0;
+static int x2ScoreTimeCount = 0;      // Timer for x2 score multiplier item
+static int x2ScoreActive = 0;         // flag of x2 score multiplier item
 
-static int enlargeTimeCount = 0;
-static int enlargeActive = 0;
+static int enlargeTimeCount = 0;      // Timer for x2 size item
+static int enlargeActive = 0;         // flag of x2 size item
 
-static int breakGameLoop = 0;
+static int breakGameLoop = 0;         // flag to indicate breaking the game loop and go back to the main menu
 
-static int current_stage_index = 0;
-const unsigned long *stages[] = {stage1, stage2, stage3};
-static const unsigned long *current_stage = stage1;
+static int current_stage_index = 0;   // index of the current stage in game loop
+const unsigned long *stages[] = {stage1, stage2, stage3}; // array to store all game stage
+static const unsigned long *current_stage = stage1;       // set the current stage to stage 1 as default, change latter when game proceed 
 
-static volatile GameObject objects[MAX_OBJECTS];
+static volatile GameObject objects[MAX_OBJECTS];          // Player object, using volatitle so that the meomory region will not be touch during software life time
 // static gameStage = 1;
 
+// Game menu loop, use as welcome screen, enter or exit the game
 void gameMenu() {
+
+    // Draw the main menu screen
     drawGameBackGround(title_start);
-    int isStart = 1;
+    int isStart = 1; // flag to check if the game is start
 
     uart_puts("\nEnter Game Menu");
     // Loop for main game menu
@@ -95,22 +98,22 @@ void gameMenu() {
 
         // Direction arrow will be replace by awsd button instead
         switch (c) {
-            case 'w':
+            case 'w': // Player current choice is play the game
                 isStart = 1;
                 drawGameBackGround(title_start);
                 // show the play game screen
                 break;
-            case 's':
+            case 's': // Player current choice is exit the game
                 isStart = 0;
                 drawGameBackGround(title_exit);
                 break;
-            case '\n':
-                if (isStart) {
+            case '\n': // Player confrim choice
+                if (isStart) { // enter the game
                     uart_puts("\nGame enter");
                     clearScreen();
                     gameLoop();
 
-                } else {
+                } else { // exit the game
                     uart_puts("\nGame exist");
                     clearScreen();
                     return;
@@ -122,35 +125,40 @@ void gameMenu() {
     }
 }
 
+// main game loop, handle all logic during the gameplay
 void gameLoop() {
+
+    // Initialize the game, drawing the current stage (default stage 1), and draw the player at starting position
     drawGameBackGround(current_stage);
     drawObject(&player);
-
+    
+    // Loop to handle game logic
     while (1) {
-        // Go back to the main menu
+        // If the flag is set, reset everything and go back to the main menu
         if (breakGameLoop) {
             breakGameLoop = 0;  // reset the flag
-            current_stage_index = 0;
-            resetGameObjects();
+            current_stage_index = 0; // reset the stage index
             changeToStage(stages[current_stage_index]);  // change to the first
                                                          // stage again
             drawGameBackGround(title_start);             // main menu
             return;
         }
+
+        // Refresh the status bar on top of the screen that display time, goal and current score after each loop
         refreshStatusBar();
 
-        // Move the balls
-        updateBalls();
-        checkCollision();
+        updateBalls(); // Update the falling game objects
+        checkCollision(); // checking the collision between player and all objects in the game
 
-        checkStageProgression();
+        checkStageProgression(); // Checking the condition of wining stage, losing stage using score
 
-        // timer
-        if (frameCount % 30 == 0) {
+        // Cacluate the time based on the current frames count
+        if (frameCount % 30 == 0) { // game run of 30 fps meaning 30 frames = 1 second
             uart_puts("\nTime Left: ");
             uart_dec(timerCount);
             timerCount--;
 
+            // If flag of x2 score and x2 size is activate, calculate the timer of them
             if (x2ScoreActive) {
                 x2ScoreTimeCount++;
             }
@@ -159,12 +167,16 @@ void gameLoop() {
             }
         }
 
+        // Check if the counter is expired for x2 score then reset both the flag and the timer
         if (x2ScoreTimeCount == MULTIPLY_TIME) {
             x2ScoreActive = 0;
             x2ScoreTimeCount = 0;
         }
 
+        // Check if the counter is expired for x2 size then reset both the flag and the timer
         if (enlargeTimeCount == ENLARGE_TIME) {
+
+            // Remove the enlarged player, change the size and sprite back to origin size then draw again
             eraseObject(&player);
             player.sprite = basketball_hoops;
             player.width = PLAYER_WIDTH;
@@ -174,18 +186,19 @@ void gameLoop() {
             enlargeTimeCount = 0;
         }
 
-        // Time limit reach then end game
+        // Time limit reached, game over
         if (timerCount == 0) {
             uart_puts("\nTime limit reached, game lose");
 
-            if (inGameMenuChoice(MENU_TAG_LOSE)) {
+            // Enter the losing menu to let player choose either to reset the current stage or exit the game
+            if (inGameMenuChoice(MENU_TAG_LOSE)) { // if player want to exit the game
                 breakGameLoop = 1;
                 continue;
-            } else {  // reset the stage
+            } else {  // if player want to rest the stage
                 breakGameLoop = 0;
-                resetGameObjects();  // Reset player
-                drawGameBackGround(current_stage);
-                drawObject(&player);
+                resetGameObjects();  // Reset all game objects
+                drawGameBackGround(current_stage); // redraw the current stage
+                drawObject(&player); // redraw the player at starting position
                 continue;
             }
         }
@@ -196,24 +209,28 @@ void gameLoop() {
             frameCount = 0;
         }
 
+        // User input handling 
         if (uart_is_read_ready()) {
             char c = uart_getc();
 
             switch (c) {
-                case 'a':
+                case 'a': // go left
                     moveObject(&player, -1, 0);
                     break;
-                case 'd':
+                case 'd': // go right
                     moveObject(&player, +1, 0);
                     break;
-                case 27:
+                case 27: // escape, pausing the game, user can exit the game or continue the game, all game object state are keeped if continue
                     if (inGameMenuChoice(MENU_TAG_PAUSE)) {
                         breakGameLoop = 1;
-                    } else {
+                    } else { // continue the game
                         breakGameLoop = 0;
+
+                        // Redraw every thing back to the state before pausing
                         drawGameBackGround(current_stage);
                         drawObject(&player);
 
+                        // Redraw all falling objects
                         for (int i = 0; i < MAX_OBJECTS; i++) {
                             if (objects[i].alive) {
                                 drawObject(&objects[i]);
@@ -226,14 +243,15 @@ void gameLoop() {
             }
         }
 
-        // Update frame count and wait
+        // Update frame count and wait for 33ms (lock the game at 30fps)
         frameCount++;
         wait_msec(33);
     }
 }
 
+// Function to get the random falling object tags for ball spawing,
 int getRandomBallType(int stage) {
-    int r = (SYS_TIMER_CLO + score) % 100;
+    int r = (SYS_TIMER_CLO + score) % 100; // randomness based on the lower system count 32 bit and current score of the player
     if (stage == 0) {
         // Stage 1 spawn rates:
         // 85% Normal, 7% Special, 4% Enlarge, 4% x2 Score
@@ -277,35 +295,40 @@ int getRandomBallType(int stage) {
     return 0;
 }
 
+// Function to check the score of current stage and condition checking for passing level
 void checkStageProgression() {
+
+    // If the score is hit at negative -100 threadhold (hit the bomb when having 0 score) then losing the game
     if (score <= -100) {
         uart_puts("\nNegative score threedhold reach, lose game");
-        if (inGameMenuChoice(MENU_TAG_LOSE)) {
+        if (inGameMenuChoice(MENU_TAG_LOSE)) { // enter the losing menu to let player decide to continue game or exit the game
             breakGameLoop = 1;
             return;
         } else {  // Continue Play the game
             breakGameLoop = 0;
-            resetGameObjects();  // Reset player
-            drawGameBackGround(current_stage);
-            drawObject(&player);
+            resetGameObjects();  // Reset all game objects
+            drawGameBackGround(current_stage); // redraw the current stage
+            drawObject(&player); // redraw player at starting position
+            return;
         }
     }
 
+    // Checking score to pass for every level
     if (score >= STAGE1_SCORE && current_stage_index == 0) {
-        current_stage_index = 1;
-        timerCount = STAGE2_SCORE;
-        changeToStage(stages[current_stage_index]);
-        drawGameBackGround(current_stage);
-        drawObject(&player);
+        current_stage_index = 1; // change index to stage 2
+        timerCount = STAGE2_SCORE; // set the time limit to stage 2 time limit
+        changeToStage(stages[current_stage_index]); // change current stage to stage 2
+        drawGameBackGround(current_stage); // draw the stage 2 background
+        drawObject(&player); // draw player at starting positioin
     } else if (score >= STAGE2_SCORE && current_stage_index == 1) {
-        current_stage_index = 2;
-        timerCount = STAGE3_SCORE;
-        changeToStage(stages[current_stage_index]);
-        drawGameBackGround(current_stage);
-        drawObject(&player);
-    } else if (score >= STAGE3_SCORE && current_stage_index == 2) {
-        breakGameLoop = 1;
-        drawGameBackGround(win_game_menu);
+        current_stage_index = 2; // change index to stage 2
+        timerCount = STAGE3_SCORE; // set the time limit to stage 3 limit
+        changeToStage(stages[current_stage_index]); // change the current stage to 3
+        drawGameBackGround(current_stage); // draw the stage 3
+        drawObject(&player); // draw player at starting position
+    } else if (score >= STAGE3_SCORE && current_stage_index == 2) { // win the stage 3 meaning wining the game
+        breakGameLoop = 1; // break the game loop flag set
+        drawGameBackGround(win_game_menu); // wining menu display
         // wait for player to confirm the winning menu
         while (1) {
             char c = uart_getc();
@@ -314,11 +337,14 @@ void checkStageProgression() {
     }
 }
 
+// Colistion checking between the player and the game objects
 void checkCollision() {
     // TEMP VARRIABLE WILL BE REMOVE IN FINAL PRODUCT
     int previousScore = score;
     // TEMP VARRIABLE WILL BE REMOVE IN FINAL PRODUCT
-    int multiplier = x2ScoreActive ? 2 : 1;
+    int multiplier = x2ScoreActive ? 2 : 1; // check if the x2 score flag is set then set the multiplier accordingly 
+
+    // loop through all the game object for collision checking
     for (int i = 0; i < MAX_BALLS; i++) {
         // only check if the ball is alive
         if (!objects[i].alive) continue;
@@ -349,15 +375,19 @@ void checkCollision() {
             }
             // enlarge
             if (objects[i].type == ENLARGE_TAG) {
+
+                // set the player sprite and player width to the enlarged one
                 player.sprite = basketball_hoops_large, player.width = 160;
+
+                // earase and redraw the player to change to larger one
                 eraseObject(&player);
                 drawObject(&player);
 
-                enlargeActive = 1;
+                enlargeActive = 1; // set the enlarger flag
             }
             // multipler
             if (objects[i].type == SCORE_MULTIPLY_TAG) {
-                x2ScoreActive = 1;
+                x2ScoreActive = 1; // set the x2 score flag
             }
         }
     }
@@ -391,30 +421,35 @@ void spawnBall() {
             else if (ball_type == SCORE_MULTIPLY_TAG)
                 sprite = x2_score;
 
+            // set up the game object according to the value get from the get random ball
             objects[i] = (GameObject){
                 .type = ball_type,
-                .x = SYS_TIMER_CLO % (SCREEN_WIDTH + score -
-                                      BALL_WIDTH),  // use system counter and
-                                                    // score as random seed
+                .x = SYS_TIMER_CLO % (SCREEN_WIDTH + score - BALL_WIDTH),  // use system counter and score as random factor to spawn 
                 .y = OBJECT_LOCATION_Y,
                 .width = BALL_WIDTH,
                 .height = (ball_type == SPEICAL_BALL_TAG) ? 80 : BALL_HEIGHT,
                 .speed = BALL_SPEED,
                 .alive = 1,
                 .sprite = sprite};
+
+            // draw the object on the screen
             drawObject(&objects[i]);
             break;
         }
     }
 }
 
-// Update the ball position
+// Update the ball position, called every game loop to move the ball
 void updateBalls() {
+
+    // only move the alive (on screen) ball
     for (int i = 0; i < MAX_BALLS; i++) {
         if (!objects[i].alive) continue;
 
         // Erase before moving
         eraseObject(&objects[i]);
+
+        // Move the object
         objects[i].y += objects[i].speed;
 
         // If ball reaches the bottom, mark as not alive and erase
@@ -426,17 +461,19 @@ void updateBalls() {
             continue;
         }
 
+        // draw object at new position
         drawObject(&objects[i]);
     }
 }
 
 // Reset all game object
 void resetGameObjects() {
-    // Reset Player
+    // Reset Player position
     player.x = PLAYER_START_X;
     player.y = PLAYER_START_Y;
     uart_puts("\nReset Player Position and Score");
 
+    // reset the time limit depend on current stage
     if (current_stage_index == 0)
         timerCount = STAGE1_TIME;
     else if (current_stage_index == 1)
@@ -444,17 +481,19 @@ void resetGameObjects() {
     else
         timerCount = STAGE3_TIME;
 
+    // reset the frame count and score
     frameCount = 0;
     score = 0;
 
+    // resest the flag, timer, player size for the enlarge feature
     enlargeActive = 0;
     enlargeTimeCount = 0;
-
     eraseObject(&player);
     player.sprite = basketball_hoops;
     player.width = PLAYER_WIDTH;
     drawObject(&player);
 
+    // reset the flag and timer for x2 score
     x2ScoreActive = 0;
     x2ScoreTimeCount = 0;
 
@@ -480,21 +519,27 @@ void drawGameBackGround(const unsigned long *bg) {
     drawImg(bg, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
 }
 
+// draw the game object on screen
 void drawObject(volatile GameObject *obj) {
     drawImg(obj->sprite, obj->y, obj->x, obj->width, obj->height, 1);
 }
 
+// move the game object
 void moveObject(volatile GameObject *obj, int dx, int dy) {
+
+    // erase before moving
     eraseObject(obj);
     obj->x += obj->speed * dx;
     obj->y += obj->speed * dy;
 
+    // move the object
     if (obj->x < 0) obj->x = 0;
     if (obj->x + obj->width > SCREEN_WIDTH) obj->x = SCREEN_WIDTH - obj->width;
     if (obj->y < 0) obj->y = 0;
     if (obj->y + obj->height > SCREEN_HEIGHT)
         obj->y = SCREEN_HEIGHT - obj->height;
 
+    // draw object at new position
     drawObject(obj);
 }
 
@@ -510,7 +555,7 @@ void eraseObject(volatile GameObject *obj) {
     }
 }
 
-// Return 0 = Continue, 1 = Exit
+// Display losing, pausing menu and handle user input
 int inGameMenuChoice(int menu_tag) {
     int choice = 0;  // 0 mean continue 1 mean exit
     // Draw the initial screen
@@ -549,11 +594,13 @@ int inGameMenuChoice(int menu_tag) {
     }
 }
 
+// refresh the status bar at the top of the screen (time, goal, socre)
 void refreshStatusBar() {
-    clearStatusBar();
-    drawStatusBar();
+    clearStatusBar(); // clear status bar before display new bar
+    drawStatusBar();  // draw new bar according to the current value
 }
 
+// clear the bar and fill back with the respected background area
 void clearStatusBar() {
     for (int y = STATUSBAR_Y_START; y < STATUSBAR_Y_START + STATUSBAR_HEIGHT;
          y++) {
@@ -564,6 +611,7 @@ void clearStatusBar() {
     }
 }
 
+// draw the status bar
 void drawStatusBar() {
     // String to store value
     char timeStr[12];
